51a2e5a6f8bf3b14f95a01e3c69a79197d4148b3
melikesukocyigit <melikesukocyigt@gmail.com>
2026-04-04 13:04:27 +0300

    Delete odev.kt

--- odev.kt
+++
@@ -1,4 +0,0 @@
-// Bank.kt dosyası oluşturalım.
-// Bir bankacılık uygulaması (OOP yok)
-// Min 5. adet bankacılık uygulamasında olması gerektiğini düşündüğünüz fonksiyonu tanımlayalım.
-// Main fonksiyonunda da bu fonksiyonları çağırıp test edelim.
